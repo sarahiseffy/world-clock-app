143dbe36f04bba44da056d758c99e810089a051a
sarahiseffy <cloudchii@gmail.com>
2024-07-14 18:16:33 +0400

    Edited HTML, JS and CSS
    Trying to simplify HTML classes

--- src/script.js
+++ src/script.js
@@ -5,7 +5,7 @@ function updateTime() {
   let losAngelesTimeElement = document.querySelector(".time1");
   let losAngelesTime = moment().tz("America/Los_Angeles");
 
-  losAngelesDateElement.innerHTML = moment().format("MMMM do, YYYY");
+  losAngelesDateElement.innerHTML = moment().format("MMMM Do, YYYY");
   losAngelesTimeElement.innerHTML = losAngelesTime.format(
     "h:mm:ss [<small>]A[</small>]"
   );
@@ -16,11 +16,31 @@ function updateTime() {
   let sydneyTimeElement = document.querySelector(".time2");
   let sydneyTime = moment().tz("Australia/Sydney");
 
-  sydneyDateElement.innerHTML = moment().format("MMMM do, YYYY");
+  sydneyDateElement.innerHTML = moment().format("MMMM Do, YYYY");
   sydneyTimeElement.innerHTML = sydneyTime.format(
     "h:mm:ss [<small>]A[</small>]"
   );
+
+  //London
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = document.querySelector(".date3");
+  let londonTimeElement = document.querySelector(".time3");
+  let londonTime = moment().tz("Europe/London");
+
+  londonDateElement.innerHTML = moment().format("MMMM Do, YYYY");
+  londonTimeElement.innerHTML = londonTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+function updateCity(event) {
+  let cityTimeZone = event.target.value; // Double check by console.log(cityTimeZone) if selected city is logged
+  let cityTime = moment().tz(cityTimeZone); // Double check by console.log(cityTime.format("MMMM Do, YYYY")) if selected city is logged
+  let citiesElement = document.querySelector("#cities");//this will replace the default displayed cities by the selected city.
+  citiesElement.innerHTML = cityTimeZone;
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
